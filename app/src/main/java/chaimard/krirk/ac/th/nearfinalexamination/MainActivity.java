fcdd11ea0cc90b660990439444e2c3d88c6bcdd9
chaimard <chaimard@email.krirk.ac.th>
2018-02-27 10:46:16 +0700

    step2

--- app/src/main/java/chaimard/krirk/ac/th/nearfinalexamination/MainActivity.java
+++ app/src/main/java/chaimard/krirk/ac/th/nearfinalexamination/MainActivity.java
@@ -3,11 +3,23 @@ package chaimard.krirk.ac.th.nearfinalexamination;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import chaimard.krirk.ac.th.nearfinalexamination.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null){
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.contentMainFragment, new MainFragment())
+                        .commit();
+
+        }
+
+
+
     }
 }
