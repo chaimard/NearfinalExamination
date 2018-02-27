8e4be6b2ea1e4ec4aa509c718d235b5799a49242
chaimard <chaimard@email.krirk.ac.th>
2018-02-27 11:03:47 +0700

    step3

--- app/src/main/java/chaimard/krirk/ac/th/nearfinalexamination/fragment/CalculateFragment.java
+++ app/src/main/java/chaimard/krirk/ac/th/nearfinalexamination/fragment/CalculateFragment.java
@@ -3,10 +3,12 @@ package chaimard.krirk.ac.th.nearfinalexamination.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import chaimard.krirk.ac.th.nearfinalexamination.MainActivity;
 import chaimard.krirk.ac.th.nearfinalexamination.R;
 
 /**
@@ -20,6 +22,21 @@ public class CalculateFragment extends Fragment{
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+//        Create Toolbar
+        Toolbar toolbar = getView().findViewById(R.id.toolbarCalculate);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+//        Exchange Controller
 
 
 
